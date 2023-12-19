99b2de94e3795bdae0aca6e394faec72e4306a7d
mariaklyass <klyassmaria@gmail.com>
2023-12-20 01:43:27 +0300

    fix: write correct path to public folder

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,10 +5,7 @@ import SocialMedia from "../socialmedia/SocialMedia";
 const Footer = () => {
   return (
     <footer id="footer" className="footer">
-      <img
-        className="footer-logo"
-        src="../../../public/images/footer-logo.jpg"
-      ></img>
+      <img className="footer-logo" src="/images/footer-logo.jpg"></img>
       <div>
         <p>Следить за новостями & слушать новую музыку:</p>
         <SocialMedia />
@@ -19,7 +16,7 @@ const Footer = () => {
           <h4>
             2023, Made by Klyass @mariaklyass{" "}
             <svg>
-              <use xlinkHref={`../../../public/icons/sprite.svg#github`}></use>
+              <use xlinkHref={`/icons/sprite.svg#github`}></use>
             </svg>{" "}
           </h4>
         </a>
--- src/components/icon/Icon.jsx
+++ src/components/icon/Icon.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Icon = ({ name, width, height }) => (
   <svg className={`icon icon-${name}`} style={{ width, height }}>
-    <use xlinkHref={`../public/icons/sprite.svg#${name}`}></use>
+    <use xlinkHref={`/icons/sprite.svg#${name}`}></use>
   </svg>
 );
 
