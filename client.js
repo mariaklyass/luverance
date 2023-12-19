c808a581527ca0f86edfee08fd14f20aa06df6c9
mariaklyass <klyassmaria@gmail.com>
2023-12-19 17:56:32 +0300

    add 404html

--- client.js
+++ client.js
@@ -3,4 +3,6 @@ import { createClient } from "@sanity/client";
 export const client = createClient({
   projectId: "lmlcsr2m",
   dataset: "production",
+  apiVersion: "2023-12-19",
+  useCdn: false,
 });
--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import { client } from "./client";
+import { client } from "../client";
 
 function App() {
   const [count, setCount] = useState(0);
